6eeefa7d564e4d016ce3fa19f2856bc581cba3d1
chrisJohns0n <126682002+ChrisJohnson0110@users.noreply.github.com>
2025-05-14 14:26:56 +0100

    status effect interactions
    handle status effect interactions

---
+++ Assets/Scripts/Levels/Grid/HandleGridInteractions.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandleGridInteractions : MonoBehaviour
+{
+    public static HandleGridInteractions instance;
+
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+        else Destroy(gameObject);
+    }
+
+    //trigger explosion
+    public void TriggerExplosion(GridTile a_gridTile)
+    {
+        foreach (BaseUnit unit in a_gridTile.UnitsOccupyingTile)
+        {
+            unit.TakeDamage(50);
+            Debug.Log($"{unit.UnitName} takes 50 damage from explosion!");
+        }
+        //remove status effects
+        a_gridTile.tileConditions.RemoveAll(c => c.State == TileState.CoveredInOil);
+    }
+}
